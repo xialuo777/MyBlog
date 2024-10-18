464bf1886a0cb93a3ccf00f39eecc01ae0133ec5
charlotte <2436056388@qq.com>
2024-10-18 09:23:28 +0800

    AdminController

--- src/main/java/com/example/controller/AdminController.java
+++ src/main/java/com/example/controller/AdminController.java
@@ -1,9 +1,18 @@
-package com.example.controller;/**
+package com.example.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
  * [一句话描述该类的功能]
  * @author       : [24360]
  * @version      : [v1.0]
  * @createTime   : [2024/10/18 9:19]
  */
-
+@RestController
+@RequiredArgsConstructor
+@Slf4j
 public class AdminController {
+
 }
